6e04d719634438c7bcf8d24b56a941f0fea32cea
sryang <sarang628@gmail.com>
2025-08-13 08:19:00 +0800

    change marker design

--- GetSelectedMarkUseCaseImpl.kt
+++ GetSelectedMarkUseCaseImpl.kt
@@ -23,7 +23,7 @@ class GetSelectedMarkUseCaseImpl {
         return object : GetSelectedMarkUseCase {
             override fun invoke(coroutineScope: CoroutineScope): StateFlow<MarkerData> {
                 return findRepository.selectedRestaurant.map {
-                    MarkerData(id = it.restaurantId, lat = it.lat, lon = it.lon, title = it.restaurantName, snippet = "", foodType = it.restaurantTypeCd)
+                    MarkerData(id = it.restaurantId, lat = it.lat, lon = it.lon, title = it.restaurantName, snippet = "", foodType = it.restaurantTypeCd, rating = "${it.rating}")
                 }.stateIn(scope = coroutineScope, started = SharingStarted.Eagerly, initialValue = MarkerData())
             }
         }
