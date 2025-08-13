89f9e9a88346d3e3b35b8edeb94a8befce0d8d95
sryang <sarang628@gmail.com>
2025-08-13 14:00:30 +0800

    add price in marker

--- GetSelectedMarkUseCaseImpl.kt
+++ GetSelectedMarkUseCaseImpl.kt
@@ -23,7 +23,7 @@ class GetSelectedMarkUseCaseImpl {
         return object : GetSelectedMarkUseCase {
             override fun invoke(coroutineScope: CoroutineScope): StateFlow<MarkerData> {
                 return findRepository.selectedRestaurant.map {
-                    MarkerData(id = it.restaurantId, lat = it.lat, lon = it.lon, title = it.restaurantName, snippet = "", foodType = it.restaurantTypeCd, rating = "${it.rating}")
+                    MarkerData(id = it.restaurantId, lat = it.lat, lon = it.lon, title = it.restaurantName, snippet = "", foodType = it.restaurantTypeCd, rating = "${it.rating}", price = it.prices)
                 }.stateIn(scope = coroutineScope, started = SharingStarted.Eagerly, initialValue = MarkerData())
             }
         }
